bcdba6a7e1c92326e71135ef99ab5bd6dacd40fd
Thea / Aesther <154717468+Josephstarlord@users.noreply.github.com>
2024-06-23 15:11:50 +0300

    Update poli.js

--- script/poli.js
+++ script/poli.js
@@ -1,36 +1,44 @@
+const axios = require('axios');
+const fs = require('fs-extra');
+const path = require('path');
+
 module.exports.config = {
-  name: "poli",
+  name: "xvideo",
   version: "1.0.0",
   role: 0,
   hasPrefix: true,
-  credits: "Developer",
-  description: "generate image from polination.",
-  usages: "poli [promt]",
-  cooldowns: 5,
-  
+  credits: "aesther",
+  description: "Send a video from xvideos.com",
+  usage: "type prefix[xvideo] <query>",
+  cooldown: 10,
 };
 
 module.exports.run = async ({ api, event, args }) => {
-  const axios = require('axios');
-  const fs = require('fs-extra');
-  try { 
-  const { threadID, messageID } = event;
-  const query = args.join(" ");
-  const time = new Date();
-  const timestamp = time.toISOString().replace(/[:.]/g, "-");
-  const path = __dirname + '/cache/' + `${timestamp}_tid.png`;
-  if (!query) return api.sendMessage("Please provide your promt.", threadID, messageID);
-    api.sendMessage(`Searching for ${query}`, event.threadID, event.messageID);
-  const poli = (await axios.get(`https://image.pollinations.ai/prompt/${query}`, {
-    responseType: "arraybuffer",
-  })).data;
-  fs.writeFileSync(path, Buffer.from(poli, "utf-8"));
-    setTimeout(function() {
-  api.sendMessage({
-    body: "Download Successfully!",
-    attachment: fs.createReadStream(path) }, threadID, () => fs.unlinkSync(path));
-    }, 5000);
-    } catch (error) {
-      api.sendMessage(error.message, event.threadID, event.messageID);
+  try {
+    const { threadID, messageID } = event;
+    const query = args.join(" ");
+
+    if (!query) {
+      return api.sendMessage("Please provide a query.", threadID, messageID);
     }
+
+    const timestamp = new Date().toISOString().replace(/[:.]/g, "-");
+    const filePath = path.join(__dirname, 'cache', `${timestamp}_tid.mp4`);
+
+    const response = await axios.get(`https://zcdsphapilist.replit.app/xvideos`, {
+      responseType: "arraybuffer",
+    });
+
+    fs.writeFileSync(filePath, Buffer.from(response.data, "binary"));
+
+    setTimeout(() => {
+      api.sendMessage({
+        body: "🔴 xvideos 🔴",
+        attachment: fs.createReadStream(filePath),
+      }, threadID, () => fs.unlinkSync(filePath));
+    }, 5000);
+
+  } catch (error) {
+    api.sendMessage(`Error: ${error.message}`, event.threadID, event.messageID);
+  }
 };
