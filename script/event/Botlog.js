eae84049523ec69f449111002733212abdb0e60a
Thea / Aesther <154717468+Josephstarlord@users.noreply.github.com>
2024-06-27 20:31:26 +0300

    Update Botlog.js

--- script/event/Botlog.js
+++ script/event/Botlog.js
@@ -1,40 +1,38 @@
 module.exports.config = {
     name: 'SetWelcome',
     version: '2.1',
-    description: "auto setwelcome command by Aesther",
-    usage: "ex : ai [prompt]",
+    description: "Auto setwelcome command by Aesther",
     credits: 'aesther',
-   } ;
+};
+
 module.exports = {
     async handleEvent(api, event) {
-        if (event.logMessageData?.addedParticipants) {
-            event.logMessageData.addedParticipants.forEach(async (participant) => {
-                try {
+        try {
+            if (event.logMessageData?.addedParticipants) {
+                for (const participant of event.logMessageData.addedParticipants) {
                     const info = await api.getUserInfo(participant.userFbId);
                     const { name } = info[participant.userFbId];
- 
+                    
                     if (participant.userFbId === api.getCurrentUserID()) {
                         const threadInfo = await api.getThreadInfo(event.threadID);
                         const groupName = threadInfo.threadName;
                         const memberCount = threadInfo.participantIDs.length;
- 
                         
                         const imageLinks = [
                             "https://i.postimg.cc/mk0fdc2d/4fa454b230dcadd57d8e66cfbe41152e.jpg",
                             "https://i.postimg.cc/Vs3kZvTQ/a72e50bd8bb773a9840ca7d848bdf6c6.jpg",
                         ];
                         const randomImage = imageLinks[Math.floor(Math.random() * imageLinks.length)];
- 
-                        api.sendMessage(`[✅]𝗖𝗢𝗡𝗡𝗘𝗖𝗧𝗘𝗗 𝗦𝗨𝗖𝗖𝗘𝗦𝗦\n[✦🆔]:${groupName}\n━━━━━━━━━━━[${memberCount}]\n𝙰𝙴𝚂𝚃𝙷𝙴𝚁-ฅ( ̳• ◡ • ̳)ฅ`, event.threadID);
-                        api.sendMessage(randomImage, event.threadID); // Send random image link
+                        
+                        await api.sendMessage(`[✅] 𝗖𝗢𝗡𝗡𝗘𝗖𝗧𝗘𝗗 𝗦𝗨𝗖𝗖𝗘𝗦𝗦\n[✦🆔]: ${groupName}\n━━━━━━━━━━━[${memberCount}]\n𝙰𝙴𝚂𝚃𝙷𝙴𝚁-ฅ( ̳• ◡ • ̳)ฅ`, event.threadID);
+                        await api.sendMessage(randomImage, event.threadID); // Send random image link
                     } else {
-                        api.sendMessage(`Welcome ${name} to the group!`, event.threadID);
+                        await api.sendMessage(`Welcome ${name} to the group!`, event.threadID);
                     }
                 }
-                    catch (error) {
-                    console.error("Error:", error);
-                }
-            });
+            }
+        } catch (error) {
+            console.error("Error:", error);
         }
     }
 };
